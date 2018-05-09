a250baacb0f29aa783e57ac8d07d32d3ba017304
chrissunny94 <chrissunny94@gmail.com>
2018-05-09 07:15:55 +0530

    New changes

--- src/subscribe.cpp
+++ src/subscribe.cpp
@@ -57,43 +57,27 @@ int estimate_vo(Mat img_1_c,Mat img_2_c){
   featureDetection(img_1, points1);        //detect features in img_1
   vector<uchar> status;
   featureTracking(img_1,img_2,points1,points2, status); //track those features to img_2
-
-  //TODO: add a fucntion to load these values directly from KITTI's calib files
-  // WARNING: different sequences in the KITTI VO dataset have different intrinsic/extrinsic parameters
   double focal = 718.8560;
   cv::Point2d pp(607.1928, 185.2157);
-  //recovering the pose and the essential matrix
   Mat E, R, t, mask;
   E = findEssentialMat(points2, points1, focal, pp, RANSAC, 0.999, 1.0, mask);
   recoverPose(E, points2, points1, R, t, focal, pp, mask);
-
   Mat prevImage = img_2;
   Mat currImage;
   vector<Point2f> prevFeatures = points2;
   vector<Point2f> currFeatures;
-
-  //char filename[100];
-
   R_f = R.clone();
   t_f = t.clone();
-
   clock_t begin = clock();
-
-  namedWindow( "Road facing camera", WINDOW_AUTOSIZE );// Create a window for display.
+  //namedWindow( "Road facing camera", WINDOW_AUTOSIZE );// Create a window for display.
   namedWindow( "Trajectory", WINDOW_AUTOSIZE );// Create a window for display.
-
   Mat traj = Mat::zeros(600, 600, CV_8UC3);
-
   Mat currImage_c = img_1_c;
   cvtColor(currImage_c, currImage, COLOR_BGR2GRAY);
   featureTracking(prevImage, currImage, prevFeatures, currFeatures, status);
-
   E = findEssentialMat(currFeatures, prevFeatures, focal, pp, RANSAC, 0.999, 1.0, mask);
   recoverPose(E, currFeatures, prevFeatures, R, t, focal, pp, mask);
-
   Mat prevPts(2,prevFeatures.size(), CV_64F), currPts(2,currFeatures.size(), CV_64F);
-
-
    for(int i=0;i<prevFeatures.size();i++) {   //this (x,y) combination makes sense as observed from the source code of triangulatePoints on GitHub
       prevPts.at<double>(0,i) = prevFeatures.at(i).x;
       prevPts.at<double>(1,i) = prevFeatures.at(i).y;
@@ -101,8 +85,6 @@ int estimate_vo(Mat img_1_c,Mat img_2_c){
       currPts.at<double>(0,i) = currFeatures.at(i).x;
       currPts.at<double>(1,i) = currFeatures.at(i).y;
     }
-
-    //scale = getAbsoluteScale(numFrame, 0, t.at<double>(2));
     scale = 2;
     cout << "Scale is " << scale << endl;
 
@@ -114,16 +96,11 @@ int estimate_vo(Mat img_1_c,Mat img_2_c){
     }
     
     else {
-     //cout << "scale below 0.1, or incorrect translation" << endl;
+     cout << "scale below 0.1, or incorrect translation" << endl;
     }
-    
-   // lines for printing results
-   // myfile << t_f.at<double>(0) << " " << t_f.at<double>(1) << " " << t_f.at<double>(2) << endl;
-
-  // a redetection is triggered in case the number of feautres being trakced go below a particular threshold
     if (prevFeatures.size() < MIN_NUM_FEAT) {
-      //cout << "Number of tracked features reduced to " << prevFeatures.size() << endl;
-      //cout << "trigerring redection" << endl;
+      cout << "Number of tracked features reduced to " << prevFeatures.size() << endl;
+      cout << "trigerring redection" << endl;
       featureDetection(prevImage, prevFeatures);
       featureTracking(prevImage,currImage,prevFeatures,currFeatures, status);
 
@@ -143,7 +120,7 @@ int estimate_vo(Mat img_1_c,Mat img_2_c){
     sprintf(text, "Coordinates: x = %02fm y = %02fm z = %02fm", t_f.at<double>(0), t_f.at<double>(1), t_f.at<double>(2));
     putText(traj, text, textOrg, fontFace, fontScale, Scalar::all(255), thickness, 8);
 
-    imshow( "Road facing camera", currImage_c );
+    //imshow( "Road facing camera", currImage_c );
     imshow( "Trajectory", traj );
 
     //waitKey(1);
@@ -153,9 +130,6 @@ int estimate_vo(Mat img_1_c,Mat img_2_c){
   clock_t end = clock();
   double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
   cout << "Total time taken: " << elapsed_secs << "s" << endl;
-
-  //cout << R_f << endl;
-  //cout << t_f << endl;
 
   return 0;
 }
